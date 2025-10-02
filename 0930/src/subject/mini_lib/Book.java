a04fa178d3eadb1e0215fe1e1b14fbf90f3376f0
junjeong <wn8624@naver.com>
2025-10-02 15:08:00 +0900

    feat: Library problem 1

---
+++ 0930/src/subject/mini_lib/Book.java
@@ -0,0 +1,15 @@
+package subject.mini_lib;
+
+public class Book {
+    // 필드(객체가 저장하고 있는 데이터) 정의
+    String title;
+    String author;
+    String isbn;
+    boolean isAvailable = true;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
